f3d9ebb7d47345f29e5c988895b33a69f9460ec9
masterkudi <cudini.andrea@gmail.com>
2023-09-13 02:07:10 +0200

    incontrato errore che non capisco

--- js/public.js
+++ js/public.js
@@ -1,10 +1,10 @@
 axios.get("api/album.php")
 .then(function (response) {
     const album_ul = document.getElementById("album_ul");
-    const posts = response.data;
+    const album = response.data;
 
-    for (let i = 0; i < posts.lenght; i++) {
-        posts_ul.innerHTML += `
+    for (let i = 0; i < album.lenght; i++) {
+        album_ul.innerHTML += `
         <li>
             <a>${album[i].title}</a>
         </li>
